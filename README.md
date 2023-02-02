b8a87a9d3123a0f5d0bef6efa2edcc6690fc6c80
Markus Appel <markusappel@hotmail.de>
2023-02-02 15:10:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# gitodo README
+
+GiToDo is a small extension that will show you all lines with uncomitted changes that contain a TODO.
+
+![Screenshot of GiToDo](https://github.com/markusa380/gitodo/media/screenshot.png)
+
+## Requirements
+
+`git` must be available as a command.
+
+## Extension Settings
+
+Currently, no settings are available.
+
+## Known Issues
+
+No known issues.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release.
---
+++ src/extension.ts
@@ -0,0 +1,186 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import { TreeItemLabel } from 'vscode';
+
+const ORIGINAL_FILE_REGEX = /--- (.*) ''(.*)''/;
+const NEW_FILE_REGEX = /\+\+\+ b\/(.*)/;
+const CHUNK_HEADER_REGEX = /@@ -(\d+)([,\d]*) \+(\d+)([,\d]*) @@ .*/;
+const ADDED_LINE_REGEX = /^\+(.*)$/;
+const REMOVED_LINE_REGEX = /^-(.*)$/;
+const UNCHANGED_LINE_REGEX = /^ (.*)$/;
+const SEARCH_WORD = "todo";
+
+export function activate(context: vscode.ExtensionContext) {
+	let provider = new GiToDoProvider();
+
+	let treeView = vscode.window.createTreeView('gitodo', {
+		treeDataProvider: provider,
+		showCollapseAll: true,
+	});
+
+	let onSave = vscode.workspace.onDidSaveTextDocument((e) => {
+		updateTreeView(provider);
+	});
+
+	updateTreeView(provider);
+
+	context.subscriptions.push(onSave);
+	context.subscriptions.push(treeView);
+}
+
+function updateTreeView(provider: GiToDoProvider) {
+	const currentDir = vscode.workspace.workspaceFolders?.[0].uri.path;
+		if (!currentDir) {
+			return;
+		}
+		
+		cp.exec(`git -C ${currentDir} diff`, (err, stdout, stderr) => {
+				if (err) {
+						console.error('git diff failed: ' + err);
+				}
+				
+				let data: GiToDoFileTreeItem[] = [];
+				let curLines: GiToDoLineTreeItem[] = [];
+				var curFile = '';
+				var curLineNumber = 0;
+				
+				stdout.split('\n').forEach((line) => {
+
+						if (line.trim().length === 0) {
+							return;
+						}
+
+						if (ORIGINAL_FILE_REGEX.test(line)) {
+							// Do nothing
+						} else if (NEW_FILE_REGEX.test(line)) {
+							// Flush old file
+							if (curLines.length > 0) {
+								data.push(new GiToDoFileTreeItem(curFile, curLines));
+							}
+							curLines = [];
+							curFile = line.match(NEW_FILE_REGEX)![1];
+						} else if (CHUNK_HEADER_REGEX.test(line)) {
+							curLineNumber = parseInt(line.match(CHUNK_HEADER_REGEX)![3]);
+						} else if (ADDED_LINE_REGEX.test(line)) {
+							let line_text = line.match(ADDED_LINE_REGEX)![1];
+							if (line_text.toLowerCase().includes(SEARCH_WORD)) {
+								curLines.push(new GiToDoLineTreeItem(curFile, line_text, curLineNumber, currentDir));
+							}
+							curLineNumber++;
+						} else if (REMOVED_LINE_REGEX.test(line)) {
+							// Do nothing
+						} else if (UNCHANGED_LINE_REGEX.test(line)) {
+							curLineNumber++;
+						} else {
+							console.info("Unexpected line matching no known pattern:", line)
+						}
+				});
+
+				if (curLines.length > 0) {
+					data.push(new GiToDoFileTreeItem(curFile, curLines));
+				}
+				
+				provider.data = data;
+				provider.refresh();
+		});
+}
+
+interface GiToDoTreeItem extends vscode.TreeItem {
+	isFile: boolean;
+}
+
+class GiToDoFileTreeItem implements GiToDoTreeItem {	
+	label: string;
+	children: GiToDoLineTreeItem[];
+	isFile: boolean = true;
+	collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.Expanded;
+	resourceUri: vscode.Uri;
+
+	constructor(file: string, children: GiToDoLineTreeItem[]) {
+		this.label = file;
+		this.children = children;
+		this.resourceUri = vscode.Uri.file("./" + file);
+	}
+}
+
+class GiToDoLineTreeItem implements GiToDoTreeItem {
+	label: TreeItemLabel;
+	line: string;
+	lineNumber: number;
+	isFile: boolean = false;
+	command: vscode.Command;
+
+	constructor(file: string, line: string, lineNumber: number, dir: string) {
+		let trimmed = line.trimStart();
+		let index = trimmed.toLowerCase().indexOf(SEARCH_WORD);
+		
+		let ellipsis = "...";
+		let resultLength = 20;
+		var result = "";
+
+		if (trimmed.length > resultLength) {
+			if (index > resultLength - SEARCH_WORD.length) {
+				result += ellipsis + " ";
+				let start_index = Math.max(
+					0,
+					index + SEARCH_WORD.length - resultLength + ellipsis.length + 1
+				);
+				result += trimmed.substring(start_index);
+			} else {
+				result = trimmed;
+			}
+		} else {
+			result = trimmed;
+		}
+
+		let prefix = `line ${lineNumber}: `;
+
+		this.label = {
+			label: prefix + result,
+			highlights: [[prefix.length + index, prefix.length + index + SEARCH_WORD.length]]
+		};
+		this.line = line;
+		this.lineNumber = lineNumber;
+
+		let open = dir + "/" + file + "#" + lineNumber;
+
+		this.command = {
+			command: 'vscode.open',
+			title: 'Open File',
+			arguments: [open],
+		};
+	}
+}
+
+class GiToDoProvider implements vscode.TreeDataProvider<GiToDoTreeItem> {
+
+	data: GiToDoFileTreeItem[] = [];
+
+	getTreeItem(element: GiToDoTreeItem): GiToDoTreeItem {
+		return element;
+	}
+
+	getChildren(element?: GiToDoTreeItem): vscode.ProviderResult<GiToDoTreeItem[]> {
+		if (element) {
+			if (element.isFile) {
+				return (element as GiToDoFileTreeItem).children;
+			} else {
+				return [];
+			}
+		} else {
+			return this.data;
+		}
+	}
+
+	private _onDidChangeTreeData: vscode.EventEmitter<GiToDoTreeItem | undefined | null | void> =
+		new vscode.EventEmitter<GiToDoTreeItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<GiToDoTreeItem | undefined | null | void> =
+		this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
